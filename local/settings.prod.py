0a47303201a0a5a3eb74be2e1e3cfe158a02c8cc
MohammedAdelAbd <116044857+MohammedAdelAbd@users.noreply.github.com>
2026-07-29 11:56:49 +0300

    dscd

--- local/settings.prod.py
+++ local/settings.prod.py
@@ -1,5 +1,9 @@
-IMAGEKIT_PUBLIC_KEY=public_3Adc3OJ6DHQ7fKiMsEGOwN+1pWQ=
-IMAGEKIT_PRIVATE_KEY=private_ST4vhFqb6uyOkiXrCrUKKjqEI5w=
-IMAGEKIT_URL_ENDPOINT=https://ik.imagekit.io/rcrickw0n/
-SECRET_KEY="django-insecure-h23ty&5-npm6zu%nuwzob-@q)n$-k8=*!k+#-lq13&)suj=o+c"
-DEBUG=False
+IMAGEKIT_PUBLIC_KEY = "public_3Adc3OJ6DHQ7fKiMsEGOwN+1pWQ="
+IMAGEKIT_PRIVATE_KEY = "private_ST4vhFqb6uyOkiXrCrUKKjqEI5w="
+IMAGEKIT_URL_ENDPOINT = "https://ik.imagekit.io/rcrickw0n/"
+SECRET_KEY = "django-insecure-h23ty&5-npm6zu%nuwzob-@q)n$-k8=*!k+#-lq13&)suj=o+c"
+DEBUG = False
+CSRF_TRUSTED_ORIGINS=["http://169.58.52.210:8000",
+                      "http://169.58.52.210:80",
+                      "http://api.makok.store"
+                     ]
